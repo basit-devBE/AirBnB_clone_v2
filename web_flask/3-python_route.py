29fb880dc7ad33923383890f54a0dfa74791c745
basit-devBE <basitmohammed362@gmail.com>
2024-03-25 15:51:00 +0000

    hello

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,7 +6,8 @@ Routes:
     /: Displays 'Hello HBNB!'
     /hbnb: Displays 'HBNB'
     /c/<text>: Displays 'C' followed by the value of the text variable
-    /python/(<text>): Displays 'Python' followed by the value of the text variable
+    /python/(<text>): Displays 'Python' followed by
+    the value of the text variable
 """
 from flask import Flask
 
@@ -18,15 +19,18 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """Displays 'C' followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -41,4 +45,3 @@ def python(text="is cool"):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-    
